789619785e9200ee918a16e70e188824a4b48a66
Ihab Saloum <ihabsaloum@Ihabs-MacBook-Pro.local>
2025-12-03 18:54:51 -0500

    chore: allow test global in ESLint for Node test runner

---
+++ tests/credit.test.js
@@ -0,0 +1,21 @@
+import assert from 'node:assert/strict';
+import {
+  calculateSolidarityCredit,
+  calculateWorkPremium,
+  calculateCWB,
+} from '../credit-calculator.js';
+
+test('calculateSolidarityCredit returns number', () => {
+  const s = calculateSolidarityCredit(30000);
+  assert.strictEqual(typeof s, 'number');
+});
+
+test('calculateWorkPremium returns number', () => {
+  const w = calculateWorkPremium(25000);
+  assert.strictEqual(typeof w, 'number');
+});
+
+test('calculateCWB returns number', () => {
+  const c = calculateCWB(20000);
+  assert.strictEqual(typeof c, 'number');
+});
---
+++ tests/rrsp.test.js
@@ -0,0 +1,13 @@
+import assert from 'node:assert/strict';
+import { calculateRrspImpact } from '../rrsp-calculator.js';
+
+test('calculateRrspImpact: no contribution', () => {
+  const res = calculateRrspImpact(50000, 0);
+  assert.strictEqual(typeof res.taxSaved, 'number');
+  assert.strictEqual(typeof res.marginalRate, 'number');
+});
+
+test('calculateRrspImpact: with contribution reduces income', () => {
+  const res = calculateRrspImpact(50000, 5000);
+  assert.ok(res.newIncome < 50000);
+});
